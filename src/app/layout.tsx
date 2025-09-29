e78c7eba905d43cc99b9d3bd9f738873f847e917
Siddhant Mahajan <s29mahaj@uwaterloo.ca>
2025-09-28 17:23:56 -0700

    v1

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import type { PropsWithChildren } from "react";
+
+import { Footer } from "@/components/main/footer";
+import { Navbar } from "@/components/main/navbar";
+import { StarsCanvas } from "@/components/main/star-background";
+// import { siteConfig } from "@/config";
+import { cn } from "@/lib/utils";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  themeColor: "#030014",
+};
+
+export const metadata: Metadata = {
+  title: "Siddhant Mahajan | Software Engineering",
+  description: "Portfolio of Siddhant Mahajan, a Computer Science graduate specializing in Artificial Intelligence, DevOps, Cloud, and Machine Learning.",
+  keywords: "Siddhant Mahajan, Software Engineer, Portfolio, Python, Tensorflow, Keras, Computer Vision, Generative AI, Cloud Engineering, Operational Excellence, Computer Science, AWS, Toronto, Brampton, Vancouver",
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "bg-[#030014] overflow-y-scroll overflow-x-hidden",
+          inter.className
+        )}
+      >
+        <StarsCanvas />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
